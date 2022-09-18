e101f27d81b9c1db8eb3ba1fbd766a3ee40a0875
lhwool0915@gmail.com <>
2022-09-19 02:31:07 +0900

    Photon pun and Joining room/ leaving room Not Completed

---
+++ Assets/Scripts/TestConnect.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class TestConnect : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Connecting to server.");
+        PhotonNetwork.NickName = MasterManager.GameSettings.NickName;
+        PhotonNetwork.GameVersion = MasterManager.GameSettings.GameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+
+    }
+
+
+    public override void OnConnectedToMaster()
+    {
+
+        Debug.Log("Connected to Photon.", this);
+
+        Debug.Log("My nickname is" + PhotonNetwork.LocalPlayer.NickName, this);
+        
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("Disconnected from server for reason" + cause.ToString(), this);
+       
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Jointed lobby");
+    }
+}
