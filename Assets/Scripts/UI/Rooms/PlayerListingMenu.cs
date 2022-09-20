22a06716ac502d8ef2a68610fb384c8a74f64fa7
lhwool0915@gmail.com <>
2022-09-21 05:33:14 +0900

    Connect Game Scene -> Completed

--- Assets/Scripts/UI/Rooms/PlayerListingMenu.cs
+++ Assets/Scripts/UI/Rooms/PlayerListingMenu.cs
@@ -140,7 +140,7 @@ public class PlayerListingMenu : MonoBehaviourPunCallbacks
             PhotonNetwork.CurrentRoom.IsOpen = false;
             // This room is no longer visible on the room list.
             PhotonNetwork.CurrentRoom.IsVisible = false;
-            PhotonNetwork.LoadLevel(1);
+            PhotonNetwork.LoadLevel(2);
         }
             
     }
