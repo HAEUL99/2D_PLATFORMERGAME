d4dc242396c96931f384d7d9b6d84856edd50a2c
aahmed7259@gmail.com <>
2022-09-28 05:27:55 +0900

    Added death behaviors

--- Assets/Scripts/PlayerScript/PlayerLife.cs
+++ Assets/Scripts/PlayerScript/PlayerLife.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerLife : MonoBehaviour
 {
+    private Rigidbody2D rb;
     private Animator anim;
 
     private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
 
@@ -18,7 +21,12 @@ public class PlayerLife : MonoBehaviour
     }
 
     private void Die() {
+        rb.bodyType = RigidbodyType2D.Static;
         anim.SetTrigger("death");
+    }
+
+    private void RestartLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
 }
