be4d606e878bb984e8e53087f8af9119984c29d1
lhwool0915@gmail.com <>
2022-09-28 03:51:33 +0900

    Just doing escButton situation

---
+++ Assets/Scripts/UI/Game/EscEvnt.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscEvnt : MonoBehaviour
+{
+    //① 이벤트 생성을 위한 대리자 하나 생성
+    public delegate void EventHandler(); //메서드를 여러 개 등록 후 호출 가능
+
+    //② 이벤트 선언: Click 이벤트
+    public event EventHandler Esc_Click;
+
+    //③ 이벤트 발생 메서드: OnClick 이벤트 처리기(핸들러) 생성
+    public void OnEscClick()
+    {
+        if (Esc_Click != null) //이벤트에 등록된 값이 있는지 확인(생략 가능)
+        {
+            Esc_Click();    //대리자 형식의 이벤트 수행
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Game/EvntManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvntManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ⓐ Button 클래스의 인스턴스 생성
+        EscEvnt _escbtn = new EscEvnt();
+
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
