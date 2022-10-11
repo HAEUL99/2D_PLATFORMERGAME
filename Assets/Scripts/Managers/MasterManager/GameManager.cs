3a09f26e70cc8fbb2ef1ae81178656121ac33ebb
lhwool0915@gmail.com <>
2022-10-12 06:39:40 +0900

    just spawn

--- Assets/Scripts/Managers/MasterManager/GameManager.cs
+++ Assets/Scripts/Managers/MasterManager/GameManager.cs
@@ -35,41 +35,8 @@ public class GameManager : MonoBehaviourPunCallbacks, IOnEventCallback
     {
         if (photonEvent.Code == 0)
         {
-
-            int[] array = (int[])photonEvent.CustomData;
-
-            for (int i = 0; i < 4; i++)
-            {
-                Debug.Log($"array{i} : {array[i]}");
-            }
-
-
-            Dictionary<int, Photon.Realtime.Player> pList = Photon.Pun.PhotonNetwork.CurrentRoom.Players;
-
-
-
-            foreach (KeyValuePair<int, Photon.Realtime.Player> p in pList)
-            {
-
-                Debug.Log($"PlayerId (not nickname) : {p.Value.ActorNumber}");
-
-                _myCustomProperties["CharNum"] = array[p.Value.ActorNumber - 1];
-
-                p.Value.CustomProperties = _myCustomProperties;
-
-
-            }
-
-
-            int num1 = (int)PhotonNetwork.LocalPlayer.CustomProperties["CharNum"];
-            Debug.Log($"num1: {num1}");
-
-            PhotonNetwork.Instantiate(obj[num1].name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
-
-
-
-
-
+            
+            PhotonNetwork.Instantiate(obj[PhotonNetwork.LocalPlayer.ActorNumber -1].name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
 
 
         }
