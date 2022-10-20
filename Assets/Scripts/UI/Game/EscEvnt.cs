8296e0073e1eeb35f0f9e459b9b8cdc4850b4e44
aahmed7259@gmail.com <>
2022-10-20 09:06:15 +0900

    Added options menu to escape menu

--- Assets/Scripts/UI/Game/EscEvnt.cs
+++ Assets/Scripts/UI/Game/EscEvnt.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class EscEvnt : MonoBehaviour
 {
@@ -25,12 +27,38 @@ public class EscEvnt : MonoBehaviour
     [SerializeField]
     private GameObject escUI;
     private bool CanseeUI;
+    public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
 
-    private void Start()
+    Resolution[] resolutions;
+
+    void Start()
     {
         escUI.SetActive(false);
         CanseeUI = false;
+
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++) {
+            string option = resolutions[i].width + " x " + resolutions[i].height + 
+                "@" + resolutions[i].refreshRate + "hz";
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && 
+                resolutions[i].height == Screen.currentResolution.height) {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
+    
 
     private void Update()
     {
@@ -66,9 +94,18 @@ public class EscEvnt : MonoBehaviour
         CanseeUI = false;
     }
 
-    public void OptionsBtn()
-    {
+    public void SetResolution(int resolutionIndex) {
+        Resolution resolution = resolutions[resolutionIndex];
 
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetVolume(float volume) {
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+
+    public void SetFullscreen(bool isFullscreen) {
+        Screen.fullScreen = isFullscreen;
     }
 
     public void LeaveBtn()
