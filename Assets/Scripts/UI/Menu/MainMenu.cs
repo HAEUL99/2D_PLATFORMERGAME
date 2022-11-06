b46b3de0cdb30d4878113eb3b0188e87dc05a415
lhwool0915@gmail.com <>
2022-11-06 14:21:43 +0900

    loading scene error fixed

--- Assets/Scripts/UI/Menu/MainMenu.cs
+++ Assets/Scripts/UI/Menu/MainMenu.cs
@@ -7,8 +7,19 @@ public class MainMenu : MonoBehaviour
 {
    
    public void PlayGame() {
-       //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-       SceneManager.LoadScene("LoadingScenBunny");
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if (PreviousInfo.instancepreviousInfo.numOfImg == 0)
+        {
+            SceneManager.LoadScene("LoadingScenFox");
+        }
+        if (PreviousInfo.instancepreviousInfo.numOfImg == 1)
+        {
+            SceneManager.LoadScene("LoadingScenCity");
+        }
+        if (PreviousInfo.instancepreviousInfo.numOfImg == 2)
+        {
+            SceneManager.LoadScene("LoadingScenBunny");
+        }
     }
 
    public void QuitGame() {
